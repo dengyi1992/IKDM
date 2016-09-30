cbd21ea3a8783fe53cdbb2f9c7ec0f4edc947b3c
deng <978548481@qq.com>
2016-09-30 17:21:48 +0800

    ingkee弹幕

---
+++ model/ingkee.js
@@ -0,0 +1,103 @@
+/**
+ * Created by deng on 16-9-30.
+ */
+var request = require("request");
+
+var uid = '127609101';
+var id = '1475222178703865';
+var token = null, time = null, nonce = null, sec = null, wsurl = null, sioIp = null;
+var options = {
+    method: 'GET',
+    url: 'http://webapi.busi.inke.cn/web/live_share_pc',
+    qs: {uid: uid, id: id}
+};
+
+request(options, function (error, response, body) {
+    if (error) return console.log(error);
+    var parse = JSON.parse(body);
+    var t = new Date().getTime() + '';
+    token = parse.data.token;
+    time = parse.data.time;
+    nonce = parse.data.nonce;
+    sec = parse.data.sec;
+    sioIp = parse.data.sio_ip;
+    var options = {
+        method: 'GET',
+        url: 'http://' + sioIp +
+        '/socket.io/1/',
+        qs: {
+            uid: '',
+            place: 'room',
+            sid: '1',
+            roomid: id,
+            token: token,
+            time: time,
+            nonce: nonce,
+            sec: sec,
+            t: t
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) return console.log(error);
+        wsurl = body.slice(0, 20);
+        startDMListen();
+
+        console.log(body);
+    });
+});
+function json_decode(str) {
+    var replace = str.replace(/\"(\w+)\":/g, "$1:");
+    return replace.slice(0,3);
+}
+function startDMListen() {
+    var WebSocketClient = require('websocket').client;
+
+    var client = new WebSocketClient();
+    client.on('connectFailed', function (error) {
+        console.log('Connect Error: ' + error.toString());
+    });
+    client.on('connect', function (connection) {
+        console.log('WebSocket Client Connected');
+        connection.on('error', function (error) {
+            console.log("Connection Error: " + error.toString());
+        });
+        connection.on('close', function () {
+            console.log('echo-protocol Connection Closed');
+        });
+        function sendHeartBeat() {
+            try {
+                if (connection.connected) {
+
+                    connection.sendUTF("2::");
+                }
+            }catch (e){
+                console.log(e.message);
+            }
+        }
+        connection.on('message', function (message) {
+           
+
+            if (message.type === 'utf8') {
+                switch (json_decode(message.utf8Data)){
+                    case "2::":
+                        sendHeartBeat();
+                        break;
+                    case "3::":
+                        // console.log("-----------" + json_decode(message.utf8Data));
+                        console.log(message.utf8Data.slice(4));
+                        break;
+                    default:break;
+                }
+
+            }
+        });
+    });
+    client.connect("ws://" + sioIp +
+        "/socket.io/1/websocket/" + wsurl +
+        "?uid=&place=room&sid=1&roomid=" + id +
+        "&token=&time=" + time +
+        "&nonce=" + nonce +
+        "&sec=" + sec);
+
+}
